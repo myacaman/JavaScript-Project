fd2b95ccb76ace9f5f029935e9c7bf8d7aa6c529
Maria Yacaman <yacamanmaria@gmail.com>
2014-02-07 20:30:45 -0700

    More changes to formatting

--- javascript/MyTimer.js
+++ javascript/MyTimer.js
@@ -1,61 +1,74 @@
 
 var timeoutLength = 10000;
 
-startTimer();
+var dialogDiv = buildDialogDiv();
 
-	function startTimer (){
+function buildDialogDiv(){
+    var myDiv = document.createElement('div');
+    myDiv.className = 'dialog';
 
-		//HTML generated for the pop up
-		var myTimer = setTimeout(function() {
+    var myDiv1 = document.createElement('div');
+    myDiv1.className = 'dialog-container';
+    myDiv.appendChild(myDiv1);
 
-		var myDiv = document.createElement('div');
-		myDiv.className = 'dialog';
+    var myDiv2 = document.createElement('div');
+    myDiv2.className = 'dialog-label';
+    myDiv2.innerText = 'Request Timeout';
+    myDiv1.appendChild(myDiv2);
 
-		var myDiv1 = document.createElement('div');
-		myDiv1.className = 'dialog-container';
-		myDiv.appendChild(myDiv1);
-
-		var myDiv2 = document.createElement('div');
-		myDiv2.className = 'dialog-label';
-		myDiv2.innerText = 'Request Timeout';
-		myDiv1.appendChild(myDiv2);
-
-		var myP = document.createElement('p');
-		myP.innerText = 'You have been in this website for 10 seconds. Click Continue to keep browsing our site; otherwise click Cancel and go to Google.';
-		myDiv1.appendChild(myP);
-
-		document.body.appendChild(myDiv);
-
-		//Modal - Done Button
-		var doneButton = document.createElement('button');
-		doneButton.className = 'done-dialog';
-		doneButton.innerText = 'Continue';
-		doneButton.id = 'doneButton';
-		myDiv1.appendChild(doneButton);
-
-		//Modal - Cancel Button
-		var cancelButton = document.createElement('button');
-		cancelButton.className = 'cancel-dialog';
-		cancelButton.innerText = 'Cancel';
-		cancelButton.id = 'cancelButton';
-		myDiv1.appendChild(cancelButton);
+    var myP = document.createElement('p');
+    myP.innerText = 'You have been in this website for 10 seconds. Click Continue to keep browsing our site; otherwise click Cancel and go to Google.';
+    myDiv1.appendChild(myP);
 
 
-		//Done Button onclick listener
-		doneButton.onclick = function() {
-			removeElementsByClass('dialog');
-			clearTimeout(myTimer);
-			startTimer();
-		};
+    //Modal - Done Button
+    var doneButton = document.createElement('button');
+    doneButton.className = 'done-dialog';
+    doneButton.innerText = 'Continue';
+    doneButton.id = 'doneButton';
+    myDiv1.appendChild(doneButton);
 
-		//Cancel Button onclick listener
-		cancelButton.addEventListener('click', function(){window.location = "http://www.google.com/";
-		});
-	}, timeoutLength)
+    //Modal - Cancel Button
+    var cancelButton = document.createElement('button');
+    cancelButton.className = 'cancel-dialog';
+    cancelButton.innerText = 'Cancel';
+    cancelButton.id = 'cancelButton';
+    myDiv1.appendChild(cancelButton);
+
+
+    //Bind Done Button onclick listener
+    doneButton.onclick = doneClicked;
+
+    //Bind Cancel Button onclick listener
+    cancelButton.onclick = cancelClicked;
+
+    return myDiv;
+}
+
+
+function cancelClicked()
+{
+    window.location = "http://www.google.com/";
+}
+
+function doneClicked()
+{
+    removeElementsByClass('dialog');
+    startTimer();
+}
+
+function startTimer ()
+{
+    //HTML generated for the pop up
+    setTimeout(function()
+    {
+        document.body.appendChild(dialogDiv);
+
+    }, timeoutLength)
 }
 
 //Function to stay in the current webpage if continue button is clicked
-removeElementsByClass = function(className) {
+function removeElementsByClass(className) {
 	elements = document.getElementsByClassName(className);
 
 	while (elements.length > 0) {
@@ -63,4 +76,5 @@ removeElementsByClass = function(className) {
 	}
 }
 
-
+//start the first timer of the page
+startTimer();
